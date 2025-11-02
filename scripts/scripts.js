7b1e383bfc7ddab4dff466a4d4b21ca501641590
Robert Vinson <bbkkssv@gmail.com>
2025-11-02 08:43:26 -0600

    test

---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+
+let numberChildren = 3;
+let partnerName = "Rebecca";
+let theLocation = "Alabama";
+let jobTitle = "Software Developer";
+
+
+console.log(`You will be a ${jobTitle} in ${theLocation}, and married to ${partnerName} with ${numberChildren} kids.`);
+
+let name = "Robert Vinson";
+let email = "robert@example.com";
+let password = "SecurePass123";
+let age = 48;
+let country = "USA";
+let monthlySalary = 5000;
+
+
+let annualSalary = monthlySalary * 12;
+
+
+console.log("Name:", name);
+console.log("Email:", email);
+console.log("Annual Salary:", "$" + annualSalary);
+
+
+radius = 5;
+areaCircle= Math.PI * radius * radius;
+console.log("Area of the circle:", areaCircle);
+
