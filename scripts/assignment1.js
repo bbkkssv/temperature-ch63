a445fbc9908b3b1311b13390a12d3fc42bc79c28
Robert Vinson <bbkkssv@gmail.com>
2025-11-03 08:16:29 -0600

    Assignment 1

---
+++ scripts/assignment1.js
@@ -0,0 +1,22 @@
+let person = "Gary";
+let object = "plunger";
+let verb = "rescue";
+let target = "goldfish";
+let place = "bathtub";
+let events = "loud fart";
+
+console.log("Funny story!");
+console.log(`Early one morning, ${person} grabbed a ${object} and decided to ${verb} the ${target} from the ${place}.`);
+console.log(`But halfway through the mission, a ${events} echoed behind him, and he questioned all his life choices.`);
+
+
+person = "Mildred";
+object = "pool noodle";
+verb = "chase";
+target = "neighbor's cat";
+place = "golf course";
+events = "raccoon stampede";
+
+console.log("Funny story part 2!");
+console.log(`Early one morning, ${person} grabbed a ${object} and decided to ${verb} the ${target} from the ${place}.`);
+console.log(`But halfway through the mission, a ${events} echoed behind them, and they questioned all their life choices.`);
